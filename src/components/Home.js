def3a11b54fdf3ea3eef25978a74e1021e401277
TINA MYERS <myers.tina515@gmail.com>
2021-03-02 14:11:14 -0800

    newsletter subscription

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,44 @@ import React from 'react';
 import ImageCarousel from './ImageCarousel';
 import Cards from './Cards';
 import Video from './Video';
+import { useState } from 'react';
+import axios from 'axios';
 
 
 const Home = () => {
+  const [emailAddress, setEmail] = useState('');
 
+  const emailSignUp = async (e) => {
+    e.preventDefault();
+    console.log('inside emailSignUp', emailAddress)
+    // const emailName = e.target.value;
+    // await setEmail(emailName);
+    // console.log('this is being set', e.target.value)
+    const email = {
+      name: emailAddress
+    }
+    console.log('this is the object: ', email)
+    await axios.post('https://tt-api-server.herokuapp.com/client', {email})
+      .then(res => {
+        console.log(res);
+        console.log(res.data)
+      })
+  }
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+    console.log('inside handleChange', emailAddress);
+  }
 
   return(
     <>
       <ImageCarousel/>
       <Cards/>
       <Video/>
+      <form onSubmit={emailSignUp}>
+        <label>Sign up for our newsletter!</label>
+        <input onChange={handleChange} type="text" default="Enter Email Address"></input>
+        <button type="submit">Sign Up</button>
+      </form>
     </>
   )
 }
