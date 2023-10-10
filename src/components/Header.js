35f11fe0c148e4c54dd90456921241fc38bcc05e
TINA MYERS <tfielder515@gmail.com>
2023-10-09 20:27:51 -0700

    fixing branding/footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
-import Logo from '../assets/LogoTT.jpg'
+import Logo from '../assets/FBFLogo.JPG'
 
 const Header = () => {
   return (
     <>
       <Navbar id="header" bg="dark" variant="dark">
         <img id="logo" alt="Tedashi Trained Logo Kettlebell" src={Logo}></img>
-        <Navbar.Brand href="/" id="heading">Tedashi Trained</Navbar.Brand>
+        {/* <Navbar.Brand href="/" id="heading">Forever Body Fitness</Navbar.Brand> */}
         <Nav className="justify-content-end" id="navBarRight">
           {/* <Nav.Item>
             <Nav.Link id="item" href="/">Home</Nav.Link>
