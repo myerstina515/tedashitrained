2c8ff18cdb696da6b28f2a3d2ec157e7ae4f4191
TINA MYERS <tfielder515@gmail.com>
2023-10-11 21:01:36 -0700

    fixing preferred contact method on kettlebell page

--- src/components/Kettlebell.js
+++ src/components/Kettlebell.js
@@ -131,7 +131,7 @@ const Kettlebell = (props) => {
                     <Card.Title>{contacts.name}</Card.Title>
                     <Card.Text id="text">ID: {contacts._id}</Card.Text>
                     <Card.Text id="text">Date Submitted: {contacts.date}</Card.Text>
-                    <Card.Text id="text">Date Submitted: {contacts.preferredContact}</Card.Text>
+                    <Card.Text id="text">Preferred Contact Method: {contacts.preferredContact}</Card.Text>
                     <Card.Text id="text">Email: {contacts.emailAddress}</Card.Text>
                     <Card.Text id="text">Newsletter: {contacts.newsletter}</Card.Text>
                     <Card.Text id="text">Phone: {contacts.phoneNumber}</Card.Text>
